5d87d11186f1b75b1d0db69c38a230d4449a25ac
RobbyLawrence <rwl19291@gmail.com>
2025-01-26 19:16:28 -0500

    changed challenge 2 solution

--- challenges/challenge2/solution.cpp
+++ challenges/challenge2/solution.cpp
@@ -1,3 +1,7 @@
+// Name: Robby Lawrence
+// Student ID: 000691931
+// NetID: rlawren9
+// Description: takes in lines of integers and returns the smallest difference between them
 #include <climits>
 #include <vector>
 #include <iostream>
@@ -9,7 +13,7 @@ int main() {
   int diff,num;
   vector<int> numbers;
   vector<pair<int,int> > min_diff_pairs;
-  int min_diff = INT_MAX;
+  int min_diff = INT_MAX; // INT_MAX ensures that ANY difference between two integers will take the new value
   while (cin >> num_int) {
     for (int i = 0;i<num_int;i++) {
       cin >> num;
@@ -19,15 +23,15 @@ int main() {
     for (size_t i = 1;i<numbers.size();i++) { // compare elements next to each other
       diff = abs(numbers[i] - numbers[i - 1]);
       if (diff < min_diff) {
-        min_diff_pairs.clear();
+        min_diff_pairs.clear(); // reset the vector and min_diff
         min_diff = diff;
-        min_diff_pairs.push_back(make_pair(numbers[i-1],numbers[i]));
+        min_diff_pairs.push_back(make_pair(numbers[i-1],numbers[i])); // add the new pair to the vector
       }
-      else if (diff == min_diff) {
+      else if (diff == min_diff) { // if the difference is the same as the current min_diff, add to vector
         min_diff_pairs.push_back(make_pair(numbers[i-1],numbers[i]));
       }
     }
-    cout << min_diff_pairs[0].first << " " << min_diff_pairs[0].second;
+    cout << min_diff_pairs[0].first << " " << min_diff_pairs[0].second; // output; if there's more than one element in the pair vector, output all of them
     if (min_diff_pairs.size() > 1) {
       for (size_t i = 1;i<min_diff_pairs.size();i++) {
         cout << " " << min_diff_pairs[i].first << " " << min_diff_pairs[i].second;

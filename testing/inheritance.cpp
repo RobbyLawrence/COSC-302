f2a2a1a10d65ec3dd36d61bf6c51b6e206b5555f
RobbyLawrence <rwl19291@gmail.com>
2025-02-25 18:42:31 -0500

    testing with proj 3

---
+++ testing/inheritance.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Parent {
+  public:
+    virtual int Function1();
+    virtual bool Function2();
+};
+
+class Child : public Parent {
+
+};
+
+int main() {
+
+}

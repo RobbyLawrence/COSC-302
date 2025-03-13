25a41e8edad563c619397ceab72e243e3c85d222
RobbyLawrence <rwl19291@gmail.com>
2025-03-12 20:22:59 -0400

    Add DNA repeating substring detector challenge

---
+++ challenges/challenge6/solution.cpp
@@ -0,0 +1,45 @@
+// Name: Robby Lawrence
+// NetID: rlawren9
+// Student ID: 000691931
+// Description: program reads in DNA strings and
+// outputs repeated substrings
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string dna;
+    unordered_map<string,int> substr_map;
+    while (cin >> dna) {
+        if (dna.length() < 9) {
+            cout << -1 << '\n';
+            continue;
+        }
+        for (size_t i = 0;i <= dna.size() - 9;i++) {
+            string substring = dna.substr(i,9);
+            unordered_map<string,int>::iterator it = substr_map.find(substring);
+            if (it == substr_map.end()) {
+                substr_map.insert(make_pair(substring,1)); // place substring in unordered map
+            }
+            else {
+                it->second++; // increment the number of occurences
+            }
+        }
+        // just check each element and output the repeated ones
+        vector<string> elements;
+        for (pair<string,int> element : substr_map) {
+            if (element.second > 1) {
+                elements.push_back(element.first);
+            }
+        }
+        sort(elements.begin(),elements.end());
+        for (string str : elements) {
+            cout << str << '\n';
+        }
+        cout << -1 << '\n';
+        substr_map.clear();
+    }
+    return 0;
+}

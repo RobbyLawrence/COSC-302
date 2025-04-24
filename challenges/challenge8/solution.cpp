d5b64b0fbe8c881a67e38e80a5acaebfe36e9326
RobbyLawrence <rwl19291@gmail.com>
2025-04-23 21:16:21 -0400

    updating everything

--- challenges/challenge8/solution.cpp
+++ challenges/challenge8/solution.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
 
 int main() {
@@ -16,29 +17,35 @@ int main() {
     int n = s_2.size();
     vector<vector<int> > vect(m + 1, vector<int>(n + 1, 0));
 
-    // initialize first row
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i < n+1; i++) { // put negatives in first row and column
         vect[0][i] = -i;
     }
-    // initialize first column
-    for (int i = 0; i <= m; i++) {
+    for (int i = 0; i < m+1; i++) {
         vect[i][0] = -i;
     }
     // load vector
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            int match = vect[i][j];
+    for (int i = 1; i < m+1; i++) {
+        for (int j = 1; j < n+1; j++) {
+            int match = vect[i-1][j-1];
             if (s_1[i] == s_2[j]) {
                 match += 1;
             }
             else {
                 match -= 1;
             }
-            int del = vect[i][j+1] - 1;
-            int ins = vect[i+1][j] - 1;
-            vect[i+1][j+1] = max({match, del, ins});
+            int top = vect[i-1][j] - 1;
+            int left = vect[i][j-1] - 1;
+            vect[i][j] = max({match, left, top});
         }
     }
+
+    for (int i = 0;i<m+1;i++) {
+        for (int j = 0;j<n+1;j++) {
+            cout << setw(2) << vect[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     // output the bottom right entry
     cout << vect[m][n] << endl;
 
